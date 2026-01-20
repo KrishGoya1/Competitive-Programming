ee5e3f094e470e6f413c61f621d6afceda5f6de5
Krish Goyal <143925721+KrishGoya1@users.noreply.github.com>
2026-01-20 16:05:00 +0530

    120

---
+++ leetcode/120.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    vector<vector<int>> dp;
+    int n;
+
+    int rec(vector<vector<int>>& t, int i, int j){
+        if(i >= n) return 0;
+        if(i == n-1){
+            return dp[i][j] = t[i][j];
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        return dp[i][j] = t[i][j] + min(
+        rec(t,i+1,j),
+        rec(t,i+1,j+1)
+        );
+
+    }
+public:
+    int minimumTotal(vector<vector<int>>& t) {
+        n = t.size();
+        dp.assign(n+1,vector<int>(n+1,-1));
+        return rec(t,0,0);
+        
+    }
+};
